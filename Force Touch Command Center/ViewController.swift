77f6e15f9259d307de069d58dc0722f1c2d88226
Case Wright <caseman912@me.com>
2015-09-24 17:00:42 -0400

    Initial Commit

---
+++ Force Touch Command Center/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Force Touch Command Center
+//
+//  Created by Case Wright on 9/24/15.
+//  Copyright © 2015 C453. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Force Touch Command CenterTests/Force_Touch_Command_CenterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Force_Touch_Command_CenterTests.swift
+//  Force Touch Command CenterTests
+//
+//  Created by Case Wright on 9/24/15.
+//  Copyright © 2015 C453. All rights reserved.
+//
+
+import XCTest
+@testable import Force_Touch_Command_Center
+
+class Force_Touch_Command_CenterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
